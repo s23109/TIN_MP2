62d9a33238eeea57925aec4402aecb8e69966971
D K <s23109@pjwstk.edu.pl>
2022-11-21 02:20:12 +0100

    Brak isForm w index.js

--- tin-projekt-kachniarz-s23109/routes/index.js
+++ tin-projekt-kachniarz-s23109/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { navLocation: 'Main' });
+  res.render('index', { navLocation: 'Main' , isForm:false });
 });
 
 module.exports = router;
