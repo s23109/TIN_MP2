c0c5da376e3c8e1962b2c67f7845c12682e4ce59
Dawid <s23109@pjwstk.edu.pl>
2022-12-04 21:37:07 +0100

    Freeze table names

--- tin-projekt-kachniarz-s23109/config/sequelize/sequelize.js
+++ tin-projekt-kachniarz-s23109/config/sequelize/sequelize.js
@@ -4,7 +4,10 @@ const sequelize = new Sequelize('tin-sequelize' , 'root' , 'root' ,
     {
         dialect: 'mysql',
         host: 'localhost',
-        port: '3306'
+        port: '3306',
+        define:{
+            freezeTableName: true
+        }
 
     });
 
