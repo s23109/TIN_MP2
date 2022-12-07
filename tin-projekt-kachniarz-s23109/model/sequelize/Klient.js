4a7eb484b9876459e43a4f3cb64e704659ca57f6
Dawid <s23109@pjwstk.edu.pl>
2022-12-07 11:40:34 +0100

    Klient - email regex

--- tin-projekt-kachniarz-s23109/model/sequelize/Klient.js
+++ tin-projekt-kachniarz-s23109/model/sequelize/Klient.js
@@ -40,15 +40,20 @@ const Klient = sequelize.define('Klient', {
     },
     email:{
         type: Sequelize.STRING,
-        isEmail: true,
         maxLength :64,
         validate:{
             checkIfEmail(){
                 if(this.email.length>0){
                     //jeśli nie puste, bo pole opcjonalne
-                    if (!this.email.isEmail){
+
+                    //bo buguje się przy ,,zwykłym isEmail''
+                    const regex = /[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i;
+
+                    if (!regex.test(this.email)){
                         throw new Error("Pole musi być adresem Email");
                     }
+
+                    //opcjonalnie tu od czy już jest unikalny ? (choć bardziej w kontrolerze)
                 }
             }
         }
