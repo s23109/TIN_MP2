65a85a9d26b81b31d5ac08adb352ac2ce3c93e8e
Dawid <s23109@pjwstk.edu.pl>
2023-01-15 16:52:23 +0100

    Mongo - model + Wstęp do Account repository

--- tin-projekt-kachniarz-s23109/model/mongodb/Account.js
+++ tin-projekt-kachniarz-s23109/model/mongodb/Account.js
@@ -1,2 +1,8 @@
 
+var account = {
+    login:null,
+    password:null,
+    kliID:null
+}
 
+module.exports = account;
--- tin-projekt-kachniarz-s23109/repository/mongodb/AccountRepository.js
+++ tin-projekt-kachniarz-s23109/repository/mongodb/AccountRepository.js
@@ -4,10 +4,30 @@ const account = require('../../model/mongodb/Account')
 
 const db = client.collection('LoginData');
 
-exports.createAccount = (acc) => {
-    db.insertOne(acc);
+exports.createAccount = async (acc) => {
+    await db.insertOne(acc);
 }
 
-exports.deleteAccount = (id) => {
+exports.accountExists = async (id) => {
+    let qbe = {kliID: id};
 
+    const query = await db.findOne(qbe);
+
+    if (query){
+        return true;
+    }
+
+    return false;
 }
+
+exports.deleteAccount = async (id) => {
+    let qbe = {kliID: id};
+
+    if (await this.accountExists(id)){
+        await db.deleteOne(qbe);
+    }else {
+        console.log("MongoDB: Account- Tried to delete non existing kliID" + id);
+    }
+    
+}
+
