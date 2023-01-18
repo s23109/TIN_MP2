43df94c0f1885581976bf307ba63cfff341d9146
Dawid <s23109@pjwstk.edu.pl>
2023-01-18 01:07:22 +0100

    Wstępna kopia formularza klient , dodana końcówka do wyświetlenia form, + podpięte pod button

--- tin-projekt-kachniarz-s23109/routes/index.js
+++ tin-projekt-kachniarz-s23109/routes/index.js
@@ -9,7 +9,18 @@ router.get('/', function(req, res, next) {
 
 router.post('/login',AuthRepo.login);
 router.get('/logout',AuthRepo.logout);
-
+router.get('/register', function (req,res,next) {
+  res.render('Subpages/Account/register_form',{
+    navLocation:'Register' ,
+    docType:'form',
+    kli: {},
+    formMode: 'createNew',
+    btnLabel: 'Dodaj',
+    formAction: '/klient/add',
+    pageTitle: 'Utwórz Konto',
+    validationErrors:[]
+  })
+})
 
 
 module.exports = router;
